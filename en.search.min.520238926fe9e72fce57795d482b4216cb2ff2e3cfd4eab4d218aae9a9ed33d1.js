c37522d8c06057ab63735e358ce2b7324b1455f1
NotWoods <NotWoods@users.noreply.github.com>
2020-11-30 03:03:17 +0000

    Publishing Site NotWoods/mockk-guidebook at 68c76e5ef24675a52f589a0b7a08e31953d59c29 on Mon Nov 30 03:03:17 UTC 2020

--- en.search.min.520238926fe9e72fce57795d482b4216cb2ff2e3cfd4eab4d218aae9a9ed33d1.js
+++ en.search.min.520238926fe9e72fce57795d482b4216cb2ff2e3cfd4eab4d218aae9a9ed33d1.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/mockk-guidebook/flexsearch.min.js');loadScript('/mockk-guidebook/en.search-data.min.ea14b643a41f2c3ff255f4d27e4eeff1b1d2302e7f85375206ae27aede336f83.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/mockk-guidebook/flexsearch.min.js');loadScript('/mockk-guidebook/en.search-data.min.40c99f735e374a480d2f6c2e233e510cbb9ad6c46942438a3e661624bbf60e6d.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
